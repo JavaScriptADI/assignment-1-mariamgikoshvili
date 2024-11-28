d91f90df907248654ea39f780e803c3cd744fd61
mariamgikoshvili <158741806+mariamgikoshvili@users.noreply.github.com>
2024-11-28 14:20:46 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-// Write your code here
+// 1. Add 2 numbers and show the result using console.log
+let a = 2
+let b = 3
+let sum = a + b
+
+console.log(sum)
+
+// 2. Multiply 3 numbers and show the result using console.log
+let c = 2
+let d = 3
+let e = 4
+
+let product = c * d * e
+
+console.log(product)
+
+// 3. Write "Hello name" using console.log (replace name with your actual name!)
+
+console.log('Hello Mariam')
+
+
+// 4. Concatenate 2 strings and show the result using console.log (string addition!) use only letters (a-z; A-Z)
+
+let string1 = "Mariam";
+let string2 = "Gikoshvili";
+console.log(string1 + " " + string2); 
